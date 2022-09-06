1f2cd66a08e59883c5c2275b527ec9057cd83607
Yana <yana.stepani@gmail.com>
2022-09-06 14:23:34 +0700

    Task 4 function added

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -2,18 +2,23 @@
 кратно ли оно единовременно 7 и 23. 14 -> нет, 46 -> нет, 161 -> да. 
 */
 
-// получение данных от пользователя:
-System.Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-// проверка условия
-if (number % 7 == 0 && number % 23 == 0)
+void NumbersMultiplice7And23()
 {
-    System.Console.WriteLine($"Число {number} кратно 7 и 23");
-}
-else 
-{
-    System.Console.WriteLine($"Число {number} не кратно 7 и 23");
+    // получение данных от пользователя:
+    System.Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    // проверка условия
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        System.Console.WriteLine($"Число {number} кратно 7 и 23");
+    }
+    else 
+    {
+        System.Console.WriteLine($"Число {number} не кратно 7 и 23");
+    }
 }
 
-
+// вызов функции
+NumbersMultiplice7And23();
+NumbersMultiplice7And23();
